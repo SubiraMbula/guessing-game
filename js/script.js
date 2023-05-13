6cbe8ba11f6eea040753c5a7e378446eb7d8bdc3
subby <subbysav123@gmail.com>
2023-05-13 20:36:24 +0300

    fetching the words from skillcrush website

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
+
+let word = "magnolia";
+let guessedLetters = [];
+let remainingGuesses = 8;
+
+/*words location*/
+
+const getWord = async function () {
+  const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+  const words = await response.text();
+  const wordArray = words.split("\n");
+  const randomIndex = Math.floor(Math.random() * wordArray.length);
+  word = wordArray[randomIndex].trim();
+  placeholder(word);
+};
